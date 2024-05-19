82e44380d50ddb75303f5959b92a57d1a978fd1e
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-05-19 17:36:52 -0500

    hora de hacer el deploy

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,14 +5,15 @@ import { v4 } from 'uuid';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBelbpv9d-SdFpiiG1ORHZ2Qh3i_ZizBC4",
-  authDomain: "xcosas-f8cbe.firebaseapp.com",
-  projectId: "xcosas-f8cbe",
-  storageBucket: "xcosas-f8cbe.appspot.com",
-  messagingSenderId: "1075337870029",
-  appId: "1:1075337870029:web:a18e430241393377b9c784",
-  measurementId: "G-CHNKC6MM4K" // Elimina esta línea para eliminar Google Analytics
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // Opcional, elimina esta línea si no necesitas Google Analytics
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
