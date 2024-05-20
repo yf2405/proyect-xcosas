7ea834b8247cfed90731b0bff6b582f4fbba3452
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-05-20 11:22:17 -0500

    arreglo de register

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useRouter } from "next/router";
+import {useRouter} from 'next/navigation'
 import { Button } from "@/components/ui/button";
 import React from "react";
 
